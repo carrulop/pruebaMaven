99eac9e67fa304db43a2266e170984be4fdc56b1
Carlos <Carlos@192.168.1.33>
2015-03-17 20:26:38 +0100

    Initial commit of project

---
+++ PruebaGit/src/prueba/Ejemplo.java
@@ -0,0 +1,17 @@
+package prueba;
+
+public class Ejemplo {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("Hola mundo");
+		
+	    Utiles.metodo1("Pepe");
+	    
+	    
+	}
+
+}
---
+++ PruebaGit/src/prueba/Utiles.java
@@ -0,0 +1,9 @@
+package prueba;
+
+public class Utiles {
+
+	public static void metodo1(String param) {
+		System.out.println("HOLA: TODO: por terminar");
+	}
+	
+}
